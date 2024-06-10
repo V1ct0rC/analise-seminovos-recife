1bf12dd72635115da1fcd5949c71713caea58187
PedroPeixinho <pedrohenriquepeixinho@gmail.com>
2024-06-10 00:42:12 -0300

    Add sequential process

--- carscraper/main.py
+++ carscraper/main.py
@@ -8,12 +8,35 @@ from carscraper.spiders.car_italiana import CarItalianaSpider
 from carscraper.spiders.car_pedragon import CarPedragonSpider
 from carscraper.spiders.car_usadosbr import CarUsadosBRSpider
 
+# settings = get_project_settings()
+# process = CrawlerProcess(settings)
+# process.crawl(CarAutonunesSpider)
+# process.crawl(CarAzzurraSpider)
+# process.crawl(CarDisnoveSpider)
+# process.crawl(CarItalianaSpider)
+# process.crawl(CarPedragonSpider)
+# process.crawl(CarUsadosBRSpider)
+# process.start()
+
+from scrapy.utils.reactor import install_reactor
+install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
+
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
+
 settings = get_project_settings()
-process = CrawlerProcess(settings)
-process.crawl(CarAutonunesSpider)
-process.crawl(CarAzzurraSpider)
-process.crawl(CarDisnoveSpider)
-process.crawl(CarItalianaSpider)
-process.crawl(CarPedragonSpider)
-process.crawl(CarUsadosBRSpider)
-process.start()
+runner = CrawlerRunner(settings)
+
+
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl(CarAutonunesSpider)
+    yield runner.crawl(CarAzzurraSpider)
+    yield runner.crawl(CarDisnoveSpider)
+    yield runner.crawl(CarItalianaSpider)
+    yield runner.crawl(CarPedragonSpider)
+    yield runner.crawl(CarUsadosBRSpider)
+    reactor.stop()
+
+crawl()
+reactor.run()
